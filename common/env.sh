2a5f8c6a016a854d52e9f875ae1d7f70d548ef1d
maciejslezak <ma.slezak2000@gmail.com>
2024-10-21 18:40:08 +0200

    initial commit

---
+++ common/env.sh
@@ -0,0 +1,5 @@
+source /cad/etc/lm_license_file
+# source /eda/cadence/2021-22/scripts/XCELIUM_21.03.009_RHELx86.sh
+# source /eda/cadence/2021-22/scripts/VMANAGER_21.03.003_RHELx86.sh
+source /eda/cadence/2023-24/scripts/XCELIUM_23.03.007_RHELx86.sh
+source /eda/cadence/2023-24/scripts/VMANAGER_23.03.004_RHELx86.sh
